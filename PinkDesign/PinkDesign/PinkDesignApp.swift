05f09887ad40fe7b44351a1ab5afaf5449b87038
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-07 18:36:25 +0300

    added the project

---
+++ PinkDesign/PinkDesign/PinkDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkDesignApp.swift
+//  PinkDesign
+//
+//  Created by Hussain Almousawi on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PinkDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
